f0e0359116eb7456a75363fe3d3814ee37df3d02
Yordi <yorditjordin@gmail.com>
2023-04-13 14:31:27 +0300

    asdas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+const todo = document.querySelector('#new-item')
+
+const con = function (b){
+    console.log('running con')
+console.log(b)
+}
+
+const load = ()=>{
+   
+        todo.addEventListener('keyup',con(todo))
+    
+}
+window.onload(load)
