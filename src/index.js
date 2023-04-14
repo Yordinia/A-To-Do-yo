93a616a76a1eb289ed978f95bbafe56db237602e
Yordi <yorditjordin@gmail.com>
2023-04-14 14:52:23 +0300

    tried a lot of this stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,67 @@
 import './style.css';
-const input = document.querySelector('#new-item')
-const form = document.querySelector('#form')
+import './styling-purposes.js'
 
+const input = document.querySelector('#new-item');
+const form = document.querySelector('form');
+const listView = document.querySelector('#todo-list');
+const newItem = document.querySelector('#new-item')
 
-const con = function (b){
-    form.preventDefault;
-    console.log('just ran con function')
-console.log(b.value)
+class Mytodo {
+  constructor() {
+    this.list = JSON.parse(localStorage.getItem('list')) || [];
+  }
+
+  addTodo(e) {
+    // console.log('hello', input, input.value, input.value.trim())
+    e.preventDefault();
+    const obj = new List(input.value.trim());
+    x.list.push(obj);
+    form.reset();
+    console.log(x.list[x.list.length-1])
+    x.render();
+  }
+
+  render() {
+    x.toLocal();
+
+    listView.innerHTML = '';
+     // Render new todo list based on updated tasks array
+    x.list.forEach((noteObj,index) => {
+    const li = document.createElement('li');
+    li.classList.add('navbar','navbar-brand')
+    li.innerHTML = `
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" id="${index}" ${
+        noteObj.completed ? 'checked' : ''
+      } data-index="${index}">
+    <label class="form-check-label" for="${index}">
+     ${noteObj.description}
+    </label>
+    </div>
+    <div>
+      <i class="bi bi-pencil"><span class="help">edit</span></i>
+      <i class="bi bi-trash2"><span class="help">delete</span></i>
+    </div>
+    `;
+    li.classList.toggle('completed', noteObj.completed);
+    listView.appendChild(li);
+  });
+  }
+
+  toLocal() {
+    localStorage.setItem('list', JSON.stringify(x.list));
+  }
 }
 
+class List {
+  constructor(description) {
+    this.id = x.list.length;
+    this.description = description;
+    this.completed = false;
+  }
+}
 
-form.addEventListener('submit',con(input))    
-
+const x = new Mytodo();
+console.log(x.list)
+x.render();
+form.addEventListener('submit', x.addTodo);
---
+++ src/styling-purposes.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#new-item')
+const form = document.querySelector('form')
+const classOns = ['mouseover','focus', 'keyup'];
+const classOffs = ['mouseout','blur' ];
+const cursorHide = ['click', 'keyup'];
+
+var hover = false;
+
+function classOn(e){
+   input.classList.toggle('on-hover', true)
+   if(e.type==='mouseover') hover= true;
+}
+
+function classOff(e){
+    if(e.type === 'mouseout'){
+        let flag = false;
+       if(hover===false || e.target.value !== '') flag = true;
+    // if(e.target.value === '' && document.hasFocus(e.target)=== true) flag = true;
+       if(flag) return
+    }
+    input.classList.toggle('on-hover', false);
+}
+
+function hideCursor(e){
+    e.target.style.cursor = 'none';
+    hover = true;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  
+    classOns.forEach(event =>
+        input.addEventListener(event, classOn))
+    classOffs.forEach( event => 
+        input.addEventListener(event, classOff))
+    cursorHide.forEach( event => 
+        input.addEventListener(event, hideCursor))
+    })
+
