a42070f47013a9c29292214f700a3fbeac61db5c
Yordi <yorditjordin@gmail.com>
2023-05-04 16:10:49 +0300

    List-Structure

--- src/code-reuse.js
+++ src/code-reuse.js
@@ -1,35 +1,7 @@
-const form = document.querySelector('form');
 const listView = document.querySelector('#todo-list');
 
 export function toLocal(todoObject) {
   localStorage.setItem('list', JSON.stringify(todoObject.list));
-}
-
-export function refreshPage() {
-  document.location.reload();
-}
-
-export function updateId(myList) {
-  myList.forEach((object, index) => {
-    object.id = index;
-  });
-  toLocal(myList);
-}
-
-export function setEventListener(list, theFunction, event) {
-  for (let i = 0; i < list.length; i += 1) {
-    list[i].addEventListener(event, theFunction);
-  }
-}
-
-export function falseInput() {
-  const falseInput = form.querySelector('#false-input');
-  form.reset();
-  falseInput.classList.replace('setVisibilityHidden', 'setVisibility');
-  setTimeout(() => {
-    falseInput.classList.replace('setVisibility', 'setVisibilityHidden');
-  }, 2500);
-  return false;
 }
 
 export function listEmpty() {
