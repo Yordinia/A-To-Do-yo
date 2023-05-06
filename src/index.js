939dcced1cfbdd4aa031acda55361310fd5bd4aa
Yordi <yorditjordin@gmail.com>
2023-05-06 10:48:07 +0300

    ammend mistake

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 /* eslint-disable no-use-before-define */
 
+import './style.css';
+import { toggleClearCompleted, toggleCheckedList, showDraggable} from '../modules/styling-purpose.js';
+
+import {
+  setEventListener, toLocal, falseInput, listEmpty, refreshPage, updateId,
+} from '../modules/code-reuse.js';
+
+import draggable from '../modules/draggable.js';
+
+
+const input = document.querySelector('#new-item');
+const form = document.querySelector('form');
 const listView = document.querySelector('#todo-list');
 
 class MyTodo {
   constructor() {
-    this.list = JSON.parse(localStorage.getItem('list')) || [{
-      id: 1,
-      description: 'Add, Delete and Edit Notes',
-      completed: false,
-    },
-    {
-      id: 1,
-      description: 'Check and Clear selected',
-      completed: false,
-    },
-    ];
+    this.list = JSON.parse(localStorage.getItem('list')) || [];
     this.checked = this.list.filter(({ completed }) => completed).length;
   }
 
   addTodo(e) {
     e.preventDefault();
+    console.log(this);
     const inputNote = input.value.trim();
     if (inputNote.length === 0) {
       falseInput(myTodo);
@@ -29,7 +32,6 @@ class MyTodo {
     myTodo.list.push(obj);
     form.reset();
     myTodo.render();
-    console.log(this);
   }
 
   removeTodo(li) {
@@ -78,11 +80,45 @@ class MyTodo {
     inputElem.addEventListener('blur', updateDescription);
   }
 
+  clearCompleted() {
+    const completedLength = myTodo.list.filter(({ completed }) => completed).length;
+    myTodo.list = myTodo.list.filter(({ completed }) => !completed);
+    myTodo.checked -= completedLength;
+    updateId(myTodo.list);
+    //toggleClearCompleted(myTodo);
+    console.log('this is clear completed - list',myTodo.list,'just cleared ', completedLength,' notes!');
+    myTodo.render();
+  }
+
+  checkBox() {
+    const li = this.parentElement.parentElement;
+    toggleCheckedList(li, this.checked);
+
+    if (this.checked) {
+      myTodo.list[this.dataset.index].completed = true;
+      myTodo.checked += 1;
+
+      if (myTodo.checked === 1) {
+      // Activate Clear Completed
+        toggleClearCompleted(myTodo);
+      }
+    } else {
+      myTodo.list[this.dataset.index].completed = false;
+      myTodo.checked -= 1;
+
+      if (myTodo.checked === 0) {
+      // Deactivate clear completed
+        toggleClearCompleted(myTodo);
+      }
+    }
+
+    toLocal(myTodo);
+  }
+
   render() {
     toLocal(myTodo);
     listView.innerHTML = '';
 
-    console.log(`Congrats${this}`); // linters told me I need to use this
     // If there's no value in the list
     if (myTodo.list.length === 0) {
       listEmpty();
@@ -94,9 +130,9 @@ class MyTodo {
       li.setAttribute('data-index', index);
       li.innerHTML = `
     <div class="form-check">
-    <input class="form-check-input" type="checkbox" id="${index}"
-     ${noteObj.completed ? 'checked' : ''} 
-     data-index=${index}>
+    <input class="form-check-input" type="checkbox" id="${index}" ${
+  noteObj.completed ? 'checked' : ''
+} data-index=${index}>
     <label class="form-check-label" for="${index}">
      ${noteObj.description}
     </label>
@@ -105,20 +141,46 @@ class MyTodo {
     <div class='icons'>
       <i class="bi bi-pencil"><span class="help">edit</span></i>
       <i class="bi bi-trash2"><span class="help">delete</span></i>
+      <i class="bi bi-three-dots-vertical setVisibilityHidden"></i>
     </div>
     `;
       listView.appendChild(li);
       toggleCheckedList(li, noteObj.completed);
     });
+
+    const checkbox = document.querySelectorAll('input[type=checkbox]');
+    const editList = document.querySelectorAll('i.bi.bi-pencil');
+    const trashList = document.querySelectorAll('i.bi.bi-trash2');
+    const enter = document.querySelectorAll('i.bi.bi-arrow-90deg-left');
+    const refresh = document.querySelectorAll('i.bi.bi-arrow-clockwise');
+    const clear = document.querySelectorAll('#archive');
+    const dragIcon =  document.querySelectorAll('.bi-three-dots-vertical')
+    const li =  document.querySelectorAll('li')
+
+    // set event listener on elements after creating them
+
+    setEventListener(checkbox, myTodo.checkBox, 'change');
+    setEventListener(trashList, myTodo.removeTodo, 'click');
+    setEventListener(editList, myTodo.editDescription, 'click');
+    setEventListener(enter, myTodo.addTodo, 'click');
+    setEventListener(refresh, refreshPage, 'click');
+    setEventListener(clear, myTodo.clearCompleted, 'click');
+    setEventListener(dragIcon, draggable, 'click');
+    setEventListener(li, showDraggable, 'click');
+
+    form.addEventListener('submit', myTodo.addTodo);
+    toggleClearCompleted(myTodo);
+    console.log('Render finished', this);
   }
 }
 
-const List = function (description) {
-  this.id = myTodo.list.length + 1;
+const List = function(description) {
+  this.id = myTodo.list.length;
   this.description = description;
   this.completed = false;
-};
+}
 
 const myTodo = new MyTodo();
+  
 
-window.addEventListener('DOMContentLoaded', () => myTodo.render());
+window.addEventListener('DOMContentLoaded', () => myTodo.render())
